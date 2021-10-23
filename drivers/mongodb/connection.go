00959c286e62d4c81e19dfc1d3a0b766222c337a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-10-23 10:58:09 -0500

    add drivers conection

---
+++ drivers/mongodb/connection.go
@@ -0,0 +1,27 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	"github.com/platzily/dealer/config"
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewConnection(uri string) *mongo.Client {
+
+	env := config.ReadMongoDBConfig()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(env.URL))
+
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %s", err)
+	}
+
+	log.Infof("Mongo Connection Success to %s", env.URL)
+
+	return client
+}
